c935dbb9dae6fbc7cffcb2d7fd10ee5078a9ef94
Daniel Porta <daniel.porta@dfki.de>
2018-06-02 22:50:09 +0200

    refinements

--- de.dfki.iui.basys.common.emf.jaxrs/src/main/java/de/dfki/iui/basys/common/emf/jaxrs/CORSResponseFilter.java
+++ de.dfki.iui.basys.common.emf.jaxrs/src/main/java/de/dfki/iui/basys/common/emf/jaxrs/CORSResponseFilter.java
@@ -14,10 +14,12 @@ import org.osgi.service.component.annotations.Component;
 @Component
 public class CORSResponseFilter implements ContainerResponseFilter {
 
+	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
 
 		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
+		headers.add("Allow-Control-Allow-Origin", "*");
 		headers.add("Access-Control-Allow-Origin", "*");
 		// headers.add("Access-Control-Allow-Origin", "http://podcastpedia.org"); //allows CORS requests only coming from podcastpedia.org
 		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
