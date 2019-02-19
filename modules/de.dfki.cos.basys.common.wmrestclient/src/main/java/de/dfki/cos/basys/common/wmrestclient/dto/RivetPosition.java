e063b48e69980954fed8079571b296b4ce4b74d0
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2019-02-19 14:20:32 +0100

    Added fields and constructors to rivet position as expected by client

--- modules/de.dfki.cos.basys.common.wmrestclient/src/main/java/de/dfki/cos/basys/common/wmrestclient/dto/RivetPosition.java
+++ modules/de.dfki.cos.basys.common.wmrestclient/src/main/java/de/dfki/cos/basys/common/wmrestclient/dto/RivetPosition.java
@@ -6,79 +6,93 @@ import de.dfki.cos.basys.common.wmrestclient.dto.Frame.FrameType;
 import de.dfki.cos.basys.common.wmrestclient.dto.Sector.SectorEnum;
 
 public class RivetPosition {
-	
-	public enum State {
-		UNDEFINED, EMPTY, INSERTING, INSERTED, CHECKING, CHECKED_IO, CHECKED_NIO, SEALING, SEALED
-	}
-		
-	// a UUID
-	String id;
-		
-	//parent frame
-	String parentId;
-	
-	// logical coordinates
-	int frameIndex; // global!
-	FrameType frameType;
-	int index;	
-	
-	// current state
-	State state;
-	
-	
 
-	public RivetPosition(int index) {
-		this(UUID.randomUUID().toString(), index);
-	}
+    public enum State {
+        UNDEFINED, EMPTY, SQUEEZING, SQUEEZED, CHECKING, CHECKED_IO, CHECKED_NIO, SEALING, SEALED
+    }
 
-	public RivetPosition(String id, int index) {
-		this.id = id;
-		this.index = index;
-		this.state = State.EMPTY;
-	}	
-	
-	public String getId() {
-		return id;
-	}
-	
-	public int getIndex() {
-		return index;
-	}
+    // a UUID
+    String id;
 
-	public int getFrameIndex() {
-		return frameIndex;
-	}
-	
-	public RivetPosition setFrameIndex(int frameIndex) {
-		this.frameIndex = frameIndex;
-		return this;
-	}
-	
-	public FrameType getFrameType() {
-		return frameType;
-	}
-	
-	public RivetPosition setFrameType(FrameType frameType) {
-		this.frameType = frameType;
-		return this;
-	}
+    //parent frame
+    String parentId;
 
-	public String getParentId() {
-		return parentId;
-	}
-	
-	public RivetPosition setParentId(String parentId) {
-		this.parentId = parentId;
-		return this;
-	}
+    // logical coordinates
+    int frameIndex; // global!
+    FrameType frameType;
+    int index;
 
-	public State getState() {
-		return state;
-	}
-	
-	public RivetPosition setState(State state) {
-		this.state = state;
-		return this;
-	}
-	
+    // current state
+    State state;
+    String stateAttributeUri;
+
+    String resourceUri;
+
+    public RivetPosition(int index) {
+        this(UUID.randomUUID().toString(), index);
+    }
+
+    public RivetPosition(String id, int index) {
+        this.id = id;
+        this.index = index;
+        this.state = State.EMPTY;
+    }
+
+    public RivetPosition(String id, String uri, int index) {
+        this(id, index);
+        resourceUri = uri;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getResourceUri() {
+        return resourceUri;
+    }
+
+    public int getFrameIndex() {
+        return frameIndex;
+    }
+
+    public RivetPosition setFrameIndex(int frameIndex) {
+        this.frameIndex = frameIndex;
+        return this;
+    }
+
+    public FrameType getFrameType() {
+        return frameType;
+    }
+
+    public RivetPosition setFrameType(FrameType frameType) {
+        this.frameType = frameType;
+        return this;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public RivetPosition setParentId(String parentId) {
+        this.parentId = parentId;
+        return this;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public RivetPosition setState(State state) {
+        this.state = state;
+        return this;
+    }
+
+    public void setStateAttributeUri(String uri) {
+        stateAttributeUri = uri;
+    }
+
 }
