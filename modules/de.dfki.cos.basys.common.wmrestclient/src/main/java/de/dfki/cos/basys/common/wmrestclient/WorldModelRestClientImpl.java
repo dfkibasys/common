88437f1ae4ae395a87260746ac52bbe98089f63d
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2019-03-06 10:16:10 +0100

    Update Rivet Position Method

--- modules/de.dfki.cos.basys.common.wmrestclient/src/main/java/de/dfki/cos/basys/common/wmrestclient/WorldModelRestClientImpl.java
+++ modules/de.dfki.cos.basys.common.wmrestclient/src/main/java/de/dfki/cos/basys/common/wmrestclient/WorldModelRestClientImpl.java
@@ -107,8 +107,14 @@ public class WorldModelRestClientImpl implements WorldModelRestClient {
 
     @Override
     public boolean updateRivetPosition(RivetPosition rivetPosition) {
-        // TODO Auto-generated method stub
-        return false;
+
+	StateResponse state = GetStateForRivet(rivetPosition.getId());
+	Client client = ClientBuilder.newClient();
+	WebTarget endpoint = client.target(state.stateUri);
+	Response response = endpoint
+		.request(MediaType.APPLICATION_JSON_TYPE)
+		.post(Entity.json(String.format("\"%d\"", rivetPosition.getState().ordinal())));
+	return response.getStatus() == 204;
     }
 
     private List<RivetPosition> PerformQueryForRivetList(String parameterizedQuery) throws URISyntaxException, IOException {
