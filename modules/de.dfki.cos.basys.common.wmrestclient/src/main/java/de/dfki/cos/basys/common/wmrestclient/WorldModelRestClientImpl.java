7c77bf5d6be2d7ab5aa36ee74c275967d607fa12
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2019-03-06 10:16:35 +0100

    Imports

--- modules/de.dfki.cos.basys.common.wmrestclient/src/main/java/de/dfki/cos/basys/common/wmrestclient/WorldModelRestClientImpl.java
+++ modules/de.dfki.cos.basys.common.wmrestclient/src/main/java/de/dfki/cos/basys/common/wmrestclient/WorldModelRestClientImpl.java
@@ -21,6 +21,12 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.LinkedList;
 import java.util.logging.Level;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class WorldModelRestClientImpl implements WorldModelRestClient {
 
