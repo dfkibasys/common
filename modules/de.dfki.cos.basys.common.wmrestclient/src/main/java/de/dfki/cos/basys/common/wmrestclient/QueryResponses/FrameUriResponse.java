ded2b502faf7b2347f7e67d69a3387a348a0cd97
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2019-03-13 13:39:58 +0100

    Added query responses for frameUris and RivetCounts

---
+++ modules/de.dfki.cos.basys.common.wmrestclient/src/main/java/de/dfki/cos/basys/common/wmrestclient/QueryResponses/FrameUriResponse.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.dfki.cos.basys.common.wmrestclient.QueryResponses;
+
+/**
+ *
+ * @author tospie
+ */
+public class FrameUriResponse {
+
+    public String frameUri;
+    public int frameIndex;
+}
