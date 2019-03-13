14be9f0551823ba120bba48a90c9a7535f97862b
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2019-03-13 16:14:41 +0100

    Added new more efficient query to query all hull data

---
+++ modules/de.dfki.cos.basys.common.wmrestclient/src/main/java/de/dfki/cos/basys/common/wmrestclient/QueryResponses/HullDataResponse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.dfki.cos.basys.common.wmrestclient.QueryResponses;
+
+import de.dfki.cos.basys.common.wmrestclient.dto.Frame.FrameType;
+import de.dfki.cos.basys.common.wmrestclient.dto.RivetPosition.State;
+import de.dfki.cos.basys.common.wmrestclient.dto.Sector.SectorEnum;
+
+/**
+ *
+ * @author tospie
+ */
+public class HullDataResponse {
+    public String rivetUri;
+    public String frameUri;
+    public String rivetId;
+    public String frameId;
+    public int frameIndex;
+    public int index;
+    public State status;
+    public SectorEnum hullRegion;
+    public FrameType frameType;
+}
